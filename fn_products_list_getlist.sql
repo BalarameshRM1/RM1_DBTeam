88ef6b8450533f9666652bc7815ea2e6170163f8
SahithaCheekatlaRM1 <sahitha.cheekatla@rm1codershub.com>
2025-11-11 20:00:25 +0530

    fn_products_list_getlist.sql
    Added order by 1.

--- fn_products_list_getlist.sql
+++ fn_products_list_getlist.sql
@@ -51,3 +51,36 @@ where (p.id = prdctid or -1=prdctid);
 
 end;
 $BODY$;
+
+---modified on 11-11-2025
+
+CREATE OR REPLACE FUNCTION public.fn_products_list_getlist(
+	prdctid bigint)
+    RETURNS TABLE(id bigint, product_name character varying, category_id bigint, category_name character varying,
+	              stock integer,
+				  price numeric,
+				  product_status_id bigint, product_status character varying,
+				  is_active boolean) 
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    ROWS 1000
+
+AS $BODY$
+
+begin 
+return query
+select p.id, p.product_name, p.category_id, c.category_name,
+p.stock,
+p.price,
+p.product_status_id, ps.product_status,
+p.is_active 
+from products p  
+left join master_category c  on p.category_id = c.id and p.is_active =true and c.is_active =true
+left join master_product_status ps  on p.product_status_id = ps.id and ps.is_active =true 
+where (p.id = prdctid or -1=prdctid)
+order by 1;
+
+end;
+$BODY$;
+
